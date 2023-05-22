aac3f624b2dd8ac4ac1fb56758ccff20d3e3499e
“reachsumit1” <harry.pappi143@gmail.com>
2023-05-22 23:46:26 +0530

    Linecamparison

---
+++ UC1/Line Comaprison 1 .cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int attendance = random.Next(2);
+        if (attendance == 1)
+        {
+            Console.WriteLine("Employee is present.");
+        }
+        else
+        {
+            Console.WriteLine("Employee is absent.");
+        }
+    }
+}
